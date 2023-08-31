1b7b8a0eb483b87e8cafd4a5c00644d6ab5d5401
vipink28 <vipinkalyan28@gmail.com>
2023-08-31 11:47:03 +0530

    Project Initialized

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navigation(props) {
+    return (
+        <div>
+            Navigation
+        </div>
+    );
+}
+
+export default Navigation;
---
+++ src/components/VideoPopup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function VideoPopup(props) {
+    return (
+        <div>
+            Video Popup
+        </div>
+    );
+}
+
+export default VideoPopup;
