5520c9270a573e68594a63f45d80ad5f2380d4e0
vipink28 <vipinkalyan28@gmail.com>
2023-09-04 11:04:12 +0530

    tv slice created

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navigation(props) {
     return (
-        <div>
-            Navigation
-        </div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="#">Expand at sm</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarsExample03">
+        <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">Link</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link disabled" aria-disabled="true">Disabled</Link>
+          </li>
+          <li className="nav-item dropdown">
+            <Link className="nav-link dropdown-toggle" to="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</Link>
+            <ul className="dropdown-menu">
+              <li><Link className="dropdown-item" to="#">Action</Link></li>
+              <li><Link className="dropdown-item" to="#">Another action</Link></li>
+              <li><Link className="dropdown-item" to="#">Something else here</Link></li>
+            </ul>
+          </li>
+        </ul>
+        <form role="search">
+          <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+        </form>
+      </div>
+    </div>
+  </nav>
     );
 }
 
---
+++ src/features/tv/tvSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from '../../helper/axios';
+import { requests } from "../../helper/requests";
+
+const initialState = {
+    netflixOriginals:{
+        data: null,
+        status: "idle",
+        error: null
+    }    
+}
+
+export const fetchNetflixOriginals = createAsyncThunk(
+    'tv/fetchNetflixOriginals',
+    async()=>{
+        const response = await axios.get(requests.getNetflixOriginals);
+        return response.data;
+    }
+);
+
+export const tvSlice = createSlice({
+    name: 'tv',
+    initialState,
+    reducers:{},
+    extraReducers: (builder)=>{
+        builder
+        .addCase(fetchNetflixOriginals.pending, (state)=>{
+            state.netflixOriginals.status = "loading"
+        })
+        .addCase(fetchNetflixOriginals.fulfilled, (state, action)=>{
+            state.netflixOriginals.status = "success";
+            state.netflixOriginals.data = action.payload;
+        })
+        .addCase(fetchNetflixOriginals.rejected, (state, action)=>{
+            state.netflixOriginals.status = "failed";
+            state.netflixOriginals.error = action.error;
+        })
+    }
+})
+
+export default tvSlice.reducer;
